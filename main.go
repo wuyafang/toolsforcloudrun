2b378fa0f2d30d7f0e223181dfe3ff48d2e1f085
wuyafang <wuyafang@huawei.com>
2022-03-15 15:36:19 +0800

    Update main.go
    加入后台线程的检查

--- main.go
+++ main.go
@@ -17,10 +17,40 @@ func GetOutBoundIP() (ip string, err error) {
 	ip = strings.Split(localAddr.String(), ":")[0]
 	return
 }
+
+
+
 func main() {
+	//得到IP地址
 	ip, err := GetOutBoundIP()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(ip)
+	
+	//开启后台进程
+        duration := 5 * time.Second
+	sleepTime := os.Getenv("SLEEP_TIME")
+	if sleepTime != "" {
+		duration, _ = time.ParseDuration(sleepTime)
+	}
+	go func() {
+		for {
+			println("background thread is running...")
+			time.Sleep(duration)
+		}
+	}
+	
+	//开启服务器
+	log.Print("helloworld: starting server...")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
